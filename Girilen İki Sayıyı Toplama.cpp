718164b3299e97e7f1548bdd6cb2e156a0f3b838
Oğuzhan Çevik <cevik.ogsan@gmail.com>
2017-05-12 18:42:14 +0300

    Create Girilen İki Sayıyı Toplama.cpp

---
+++ Girilen İki Sayıyı Toplama.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+
+void main () 
+{ 
+
+	int Sayi1, Sayi2;
+	cout << "Birinci Sayiyi Giriniz = ";
+	cin >> Sayi1;
+	cout << "Ikinci Sayiyi Giriniz = ";
+	cin >> Sayi2;
+	cout << "Girilen Sayilarin\"Toplami\" = " << Sayi1 + Sayi2 
+		 << " dir/dIr" << endl;
+	system("pause"); 
+}
